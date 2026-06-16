796697939c30ec5b72eb5f88eff10c74c55d63ab
Rudransh Mishra <rudranshmishra153@gmail.com>
2026-06-16 18:33:30 +0530

    CORS

--- app.js
+++ app.js
@@ -24,7 +24,10 @@ const app = express();
 // Enable CORS with Credentials for JWT Cookie Sharing
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://bharat-frontend-nil.vercel.app'
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
