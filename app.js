ab13d93054329907049fb90761f93efc9bf61b83
Rudransh Mishra <rudranshmishra153@gmail.com>
2026-06-16 18:48:52 +0530

    CORS Final

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(
   cors({
     origin: [
       'http://localhost:5173',
-      'https://bharat-frontend-nil.vercel.app'
+      'https://bharat-frontend-nu.vercel.app'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
